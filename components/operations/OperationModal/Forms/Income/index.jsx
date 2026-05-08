99b2ee7614324ac63e5df755f0256cfcf3a479ef
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2026-05-08 19:49:37 +0500

    new changes change selleing deal id to sales_transaction_id

--- components/operations/OperationModal/Forms/Income/index.jsx
+++ components/operations/OperationModal/Forms/Income/index.jsx
@@ -268,7 +268,7 @@ const IncomeForm = observer(({
         counterparty: raw?.counterparties_id || preselectedCounterparty || null,
         chartOfAccount: raw?.chart_of_accounts_id || chart_of_accounts_id || null, // Simplified logic
         paymentType: appStore.isPayment ? 'cash' : null,
-        salesDeal: raw?.selling_deal_id || defaultDealGuid || null,
+        salesDeal: raw?.sales_transactions_id || defaultDealGuid || null,
         purpose: raw?.opisanie || '',
         currency: raw?.currenies_id || null,
       }
