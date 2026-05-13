88a057e343214479cf966c69c0001a918fe2490a
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2026-05-13 15:17:45 +0500

    remove auth required from payment

--- middleware.js
+++ middleware.js
@@ -1,17 +1,19 @@
 import { NextResponse } from 'next/server'
 
+const includedPaths = ['auth', 'payment']
+
 export function middleware(request) {
   const { pathname } = request.nextUrl
 
-  // Allow login page
-  if (pathname === '/auth' || pathname === '/payment') {
+  // Allow paths that don't require auth
+  if (includedPaths.some(path => pathname.startsWith(`/${path}`))) {
     return NextResponse.next()
   }
 
   // Check authentication for all other pages
   const isAuthenticated = request.cookies.get('isAuthenticated')?.value === 'true'
 
-  if (!isAuthenticated && (pathname !== '/auth' || pathname !== '/payment')) {
+  if (!isAuthenticated) {
     return NextResponse.redirect(new URL('/auth', request.url))
   }
 
